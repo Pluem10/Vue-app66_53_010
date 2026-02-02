223dc72e01846dbbe0a56dc60a7b5c0ef6282e5f
Pluem10 <664259010@webmail.npru.ac.th>
2026-02-02 10:32:33 +0700

    NewBase"

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue-app66_53_010/',
+  base: '/vue-app66_53_010/',
   plugins: [vue()],
   resolve: {
     alias: {
